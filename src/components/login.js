d5098dd9ec9000076bcb47a8be26ec98406fb9b4
Thendralk <thendral22.kabilan@gmail.com>
2024-04-11 22:12:48 +0530

    1st commit

---
+++ src/components/login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Login = (props) => {
+   return (
+   <Container>
+      <Content>
+         <BgImage/>
+      </Content>
+      </Container>
+   );
+};
+
+const Container =styled.section`
+overflow: hidden;
+display: flex;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
+
+const Content = styled.div`
+margin-bottom: 10vw;
+width: 100%;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 80px 40px;
+height: 100%;
+`;
+
+const BgImage = styled.div`
+background-repeat: no-repeat;
+background-image: url("public/images/login-background_disney+.jpg");
+position:absolute;
+top:0;
+right:0;
+left: 0;
+z-index: -1;
+`;
+
+export default Login;
+
---
+++ src/components/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const BgImage = styled.div`
+background-repeat: no-repeat;
+background-image: url("public/images/login-background_disney+.jpg");
+position:absolute;
+top:0;
+right:0;
+left: 0;
+z-index: -1;
+`;
